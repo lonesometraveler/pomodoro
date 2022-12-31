5c73c16e720986243318a5603843b06ae9b000ef
Kentaro Okuda <lonesometraveler@mac.com>
2022-12-31 08:48:49 -0500

    fix scan response data

--- src/lib/ble/server.rs
+++ src/lib/ble/server.rs
@@ -4,20 +4,32 @@ use crate::types::CONFIG_SIGNAL;
 use defmt::*;
 use nrf_softdevice::{
     ble::{gatt_server, peripheral},
+    raw::{
+        BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE, BLE_GAP_AD_TYPE_128BIT_SERVICE_UUID_COMPLETE,
+        BLE_GAP_AD_TYPE_COMPLETE_LOCAL_NAME, BLE_GAP_AD_TYPE_FLAGS,
+    },
     Softdevice,
 };
 
 /// BLE advertising data
 #[rustfmt::skip]
-const ADV_DATA: &[u8; 18] =
+const ADV_DATA: &[u8; 14] =
     &[
-        0x02, 0x01, nrf_softdevice::raw::BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE as u8,
-        0x03, 0x03, 0x09, 0x18,
-        0x0a, 0x09, b'P', b'o', b'm', b'o', b'd', b'o', b'r', b'o', b'!'
+        0x02, BLE_GAP_AD_TYPE_FLAGS as u8, BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE as u8,
+        0x0a, BLE_GAP_AD_TYPE_COMPLETE_LOCAL_NAME as u8, b'P', b'o', b'm', b'o', b'd', b'o', b'r', b'o', b'!'
     ];
 
-/// BLE scan data
-const SCAN_DATA: &[u8; 4] = &[0x03, 0x03, 0x09, 0x18];
+/// BLE scan response data
+#[rustfmt::skip]
+const SCAN_RESPONSE_DATA: &[u8; 18] = &[
+    // AD length
+    0x11, 
+    // AD type
+    BLE_GAP_AD_TYPE_128BIT_SERVICE_UUID_COMPLETE as u8,
+    // AD data
+    // Configuration service UUID: 11111111-1111-1111-1111-111111111122. This has to be sent in little endian order.
+    0x22, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11,
+];
 
 /// BLE GATT server
 #[nrf_softdevice::gatt_server]
@@ -34,7 +46,7 @@ pub async fn ble_server_task(server: Server, sd: &'static Softdevice) {
     let config = peripheral::Config::default();
     let adv = peripheral::ConnectableAdvertisement::ScannableUndirected {
         adv_data: ADV_DATA,
-        scan_data: SCAN_DATA,
+        scan_data: SCAN_RESPONSE_DATA,
     };
 
     loop {
